98061631272fad7671a437925923566b6f5c5943
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-07-17 04:05:52 +0300

    generate yearly report

--- resources/script/index.js
+++ resources/script/index.js
@@ -91,7 +91,114 @@ document.getElementById('subButton').onclick = function(){
     prodCostDay = (otherDay + wagesDay + feedsDay + landRateDay + medCostDay + serviceFeeDay)
     document.getElementById('productionCost').innerHTML = Math.floor(prodCostDay)
 }
-document.getElementById('subButton').onclick = function(){
-    var year = document.getElementById('yearForecast').value;
-    console.log(year)
+
+/* ============================Forecast==================================== */
+/* ==============Weekly forecast================ */
+document.getElementById('button5_1').onclick = function(){
+    var year = prompt("Input to forecast", '2022')
+    var year = prompt("Input to forecast", '2022')
+    var bpMajor = document.getElementById('majorPrice').value;
+    var bpMinor = document.getElementById('minorPrice').value;
+    var majorMilk = document.getElementById('majorLitre').value;
+    var minorMilk = document.getElementById('minorLitre').value;
+    var jerseyAvrProd = document.getElementById('jerseyMilk').value;
+    var fresianAvrProd = document.getElementById('fresianMilk').value;
+    var fresianCows = document.getElementById('fresianCows').innerHTML;
+    var jerseyCows =document.getElementById('jerseyCows').innerHTML;
+    var jerseyCowsDayMilk =(parseInt(jerseyCows) * parseInt(jerseyAvrProd))
+    var fresianCowDayMilk = (parseInt(fresianCows) * parseInt(fresianAvrProd))
+    var cowTotalDayMilk = (jerseyCowsDayMilk + fresianCowDayMilk)
+    var totalMilk = (parseInt(majorMilk) + parseInt(minorMilk))
+    if(totalMilk > cowTotalDayMilk){
+        alert('Milk sold cannot be greater than milk produced!')
+    }
+    else{
+        
+    }
+}
+
+/* ==============Monthly forecast================ */
+document.getElementById('button5_2').onclick = function(){
+    var monthsFull = {january :31, february:28, march:31 , april:30, may:31, june:30, july:31, august:31, september:30, october:31, november:30, december:31}
+    var monthsLeap = {january :31, february:29, march:31 , april:30, may:31, june:30, july:31, august:31, september:30, october:31, november:30, december:31}
+    var year = prompt("Input to forecast", '2022')
+    var bpMajor = document.getElementById('majorPrice').value;
+    var bpMinor = document.getElementById('minorPrice').value;
+    var majorMilk = document.getElementById('majorLitre').value;
+    var minorMilk = document.getElementById('minorLitre').value;
+    var jerseyAvrProd = document.getElementById('jerseyMilk').value;
+    var fresianAvrProd = document.getElementById('fresianMilk').value;
+    var fresianCows = document.getElementById('fresianCows').innerHTML;
+    var jerseyCows =document.getElementById('jerseyCows').innerHTML;
+    var jerseyCowsDayMilk =(parseInt(jerseyCows) * parseInt(jerseyAvrProd))
+    var fresianCowDayMilk = (parseInt(fresianCows) * parseInt(fresianAvrProd))
+    var cowTotalDayMilk = (jerseyCowsDayMilk + fresianCowDayMilk)
+    var totalMilk = (parseInt(majorMilk) + parseInt(minorMilk))
+    if(totalMilk > cowTotalDayMilk){
+        alert('Milk sold cannot be greater than milk produced!')
+    }
+    else{
+        
+    }
+}
+
+/* ==============Yearly forecast================ */
+document.getElementById('button5_3').onclick = function(){
+    var monthsFull = {january :31, february:28, march:31 , april:30, may:31, june:30, july:31, august:31, september:30, october:31, november:30, december:31}
+    var monthsLeap = {january :31, february:29, march:31 , april:30, may:31, june:30, july:31, august:31, september:30, october:31, november:30, december:31}
+    var year = prompt("Input to forecast", '2022')
+    var bpMajor = document.getElementById('majorPrice').value;
+    var bpMinor = document.getElementById('minorPrice').value;
+    var majorMilk = document.getElementById('majorLitre').value;
+    var minorMilk = document.getElementById('minorLitre').value;
+    var jerseyAvrProd = document.getElementById('jerseyMilk').value;
+    var fresianAvrProd = document.getElementById('fresianMilk').value;
+    var fresianCows = document.getElementById('fresianCows').innerHTML;
+    var jerseyCows =document.getElementById('jerseyCows').innerHTML;
+    var jerseyCowsDayMilk =(parseInt(jerseyCows) * parseInt(jerseyAvrProd))
+    var fresianCowDayMilk = (parseInt(fresianCows) * parseInt(fresianAvrProd))
+    var cowTotalDayMilk = (jerseyCowsDayMilk + fresianCowDayMilk)
+    var totalMilk = (parseInt(majorMilk) + parseInt(minorMilk))
+    if(totalMilk > cowTotalDayMilk){
+        alert('Milk sold cannot be greater than milk produced!')
+    }
+    else{
+        var yearSplit = parseInt(year.split(-2))
+        if (yearSplit%4 == 0){
+            var totalDays = 0
+            var leapYear = Object.values(monthsLeap)
+            for(var i in leapYear){
+                totalDays += parseInt(leapYear[i])
+            }
+            let fresianMilkYear = (totalDays * parseInt(fresianAvrProd) * parseInt(fresianCows))
+            let jerseyMilkYear = (totalDays * parseInt(jerseyAvrProd) * parseInt(jerseyCows))
+            let milkTotalYear = fresianMilkYear + jerseyMilkYear
+            //milk produced yearly
+            document.getElementById('yearTotalMilk').innerHTML = milkTotalYear
+            //milk bought yearly
+            let minorMajorYear = (totalDays * parseInt(majorMilk)) + (totalDays * parseInt(minorMilk))
+            document.getElementById('minorMajor').innerHTML = minorMajorYear
+            //total revenue generated
+            let revenueYear = minorMajorYear * parseInt(bpMajor)
+            document.getElementById('revenueYear').innerHTML = revenueYear
+        }
+        else{
+            var totalDays = 0
+            var fullYear = Object.values(monthsFull)
+            for(var i in fullYear){
+                totalDays += parseInt(fullYear[i])
+            }
+            let fresianMilkYear = (totalDays * parseInt(fresianAvrProd) * parseInt(fresianCows))
+            let jerseyMilkYear = (totalDays * parseInt(jerseyAvrProd) * parseInt(jerseyCows))
+            let milkTotalYear = fresianMilkYear + jerseyMilkYear
+            //milk produced yearly
+            document.getElementById('yearTotalMilk').innerHTML = milkTotalYear
+            //milk bought yearly
+            let minorMajorYear = (totalDays * parseInt(majorMilk)) + (totalDays * parseInt(minorMilk))
+            document.getElementById('minorMajor').innerHTML = minorMajorYear
+            //total revenue generated
+            let revenueYear = minorMajorYear * parseInt(bpMajor)
+            document.getElementById('revenueYear').innerHTML = revenueYear
+        }
+    }
 }
